e5209eb0b635c427eb4687053f353207b0f40ad1
HakanK003 <hkaratashk3@gmail.com>
2022-08-30 14:54:11 -0400

    Road update

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const canvas=document.getElementById("myCanvas");
 canvas.width=200;
 
 const ctx = canvas.getContext("2d");
-const car=new Car(100,100,30,50);
+const road = new Road(canvas.width/2, canvas.width*0.9); //*0.9 is for have a slightly empty part
+const car=new Car(road.getLaneCenter(1),100,30,50); //car will be in lane 2 (index start from 0)
 
 animate();
 
@@ -11,7 +12,16 @@ function animate(){
     car.update();
     
     canvas.height=window.innerHeight;
+
+    //looks like there is a camera up of the car follows car
+    ctx.save();
+    ctx.translate(0, -car.y+canvas.height*0.7);//*0.7 for determine where will be the car on screen 
+
+    road.draw(ctx);
     car.draw(ctx);
-    requestAnimationFrame(animate);
+
+    ctx.restore();
     
+    requestAnimationFrame(animate);
+
 }
